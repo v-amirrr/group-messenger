a6d28ac8340f909f683a4fd609bbf50040ac4425
Amir Valizadeh <79039612+v-amirrr@users.noreply.github.com>
2022-08-21 19:45:15 +0430

    feat: built redux store

--- src/index.js
+++ src/index.js
@@ -4,5 +4,12 @@ import './index.css';
 
 import App from './App';
 
+import { Provider } from 'react-redux';
+import { store } from "./redux/store";
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+);

b8b8127aa2d5c02581a92b3169603d21d34bfa0d
Amir Valizadeh <79039612+v-amirrr@users.noreply.github.com>
2022-08-24 20:36:31 +0430

    feat: changed some styles
    Now the message align sets base on what language you're typing with.
    Also the background has changed. And border radiuses too.
    And now the app gives your message its own specific font base on the language you typed your message with.
    If your message is typed with a right-to-left language it'll have an Arabic font
    and if it's typed with a left-to-right language it'll have an English font.
    Also the messages look way better now with glassmorphism effect on them.
    Also background on phone is different from the one on desktop.
    The desktop one is horizontal and the phone one is vertical.

--- src/components/Messenger.js
+++ src/components/Messenger.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 import { db } from '../config/firebase';
 import { collection, addDoc, serverTimestamp } from "firebase/firestore";
@@ -36,8 +36,11 @@ const Messenger = () => {
     const ref = collection(db, 'messages');
     const username = localStorage.getItem("username");
 
+    const inputRef = useRef();
+
     const sendMessage = e => {
         e.preventDefault();
+        inputRef.current.focus();
 
         addDoc(ref, {
             message: input,
@@ -49,6 +52,14 @@ const Messenger = () => {
         });
 
         setInput("");
+        inputRef.current.focus();
+    };
+
+    const isRTL = (text) => {           
+        let ltrChars = 'A-Za-z\u00C0-\u00D6\u00D8-\u00F6\u00F8-\u02B8\u0300-\u0590\u0800-\u1FFF'+'\u2C00-\uFB1C\uFDFE-\uFE6F\uFEFD-\uFFFF',
+            rtlChars = '\u0591-\u07FF\uFB1D-\uFDFD\uFE70-\uFEFC',
+            rtlDirCheck = new RegExp('^[^'+ltrChars+']*['+rtlChars+']');
+        return rtlDirCheck.test(text);
     };
 
     useEffect(() => {
@@ -67,8 +78,8 @@ const Messenger = () => {
                         <FlipMove>
                             {
                                 messages?.messages?.map(item => (
-                                    <Message key={item.id} isUser={item.username == JSON.parse(username)}>
-                                        <p className='username'>{item.username}:</p>
+                                    <Message key={item.id} isUser={item.username == JSON.parse(username)} isPersian={isRTL(item.message)}>
+                                        <p className='username' dir="auto">{item.username}</p>
                                         <p className='message'>{item.message}</p>
                                     </Message>
                                 ))
@@ -85,6 +96,8 @@ const Messenger = () => {
                                 onChange={(e) => setInput(e.target.value)} 
                                 placeholder="Write a Message..." 
                                 autoFocus
+                                ref={inputRef}
+                                dir="auto"
                             />
                             <motion.button whileTap={input && { scale: 0.5 }} type="submit" className='messenger-submit' disabled={!input} onClick={sendMessage}><IoSend /></motion.button>
                         </form>
@@ -102,7 +115,7 @@ const MessengerPage = styled.section`
     justify-content: center;
     align-items: center;
     flex-direction: column;
-    background-color: #000000aa;
+    background-color: #00000066;
 `;
 
 const MessengerContainer = styled.div`
@@ -116,7 +129,19 @@ const MessengerContainer = styled.div`
     padding: .5rem 0;
     font-family: 'Outfit', sans-serif;
 
+    @media (max-width: 1100px) {
+        width: 70%;
+    }
+
+    @media (max-width: 900px) {
+        width: 75%;
+    }
+
     @media (max-width: 700px) {
+        width: 80%;
+    }
+
+    @media (max-width: 500px) {
         width: 90%;
     }
 `;
@@ -133,15 +158,19 @@ const MessengerTitle = styled(motion.div)`
 
 const MessengerInput = styled(motion.div)`
     background-color: #ffffff08;
-    border-radius: 60px;
+    border-radius: 20px;
     display: flex;
     justify-content: center;
     align-items: center;
-    
+    width: 70%;
+    backdrop-filter: blur(50px) saturate(180%);
+    -webkit-backdrop-filter: blur(50px) saturate(180%);
+
     form {
         display: flex;
-        justify-content: center;
+        justify-content: space-between;
         align-items: center;
+        width: 100%;
     }
 
     .messenger-input {
@@ -158,7 +187,7 @@ const MessengerInput = styled(motion.div)`
         font-size: 1.5rem;
         width: 3rem;
         height: 3rem;
-        border-radius: 50%;
+        border-radius: 20px;
         display: flex;
         justify-content: center;
         align-items: center;
@@ -208,37 +237,32 @@ const Message = styled(motion.div)`
     justify-content: center;
     align-items: center;
     flex-direction: row;
-    background-color: ${props => props.isUser ? "#ffffff10" : "#ffffff16"};
+    background-color: ${props => props.isUser ? "#ffffff10" : "#ffffff15"};
     margin: .4rem 0;
     margin-left: ${props => props.isUser && "auto"};
     padding: .5rem .8rem; 
     border-radius: 20px;
     width: fit-content;
     max-width: 80%;
-    
+    backdrop-filter: blur(50px) saturate(150%);
+    -webkit-backdrop-filter: blur(50px) saturate(150%);
+    font-weight: 200;
+    word-break: break-all;
+
     .username {
         display: ${props => props.isUser ? "none" : ""};
-        color: #ccc;
+        color: #aaa;
         font-size: .8rem;
         margin-right: .5rem;
+        word-break: break-all;
     }
 
     .message {
-        text-align: left;
-        word-spacing: 2px;
-    }
-
-    @media (max-width: 700px) {
-        padding: .4rem .6rem;
-        margin-left: ${props => props.isUser && "auto"};
-
-        .username {
-            font-size: .4rem;
-        }
-
-        .message {
-            font-size: .7rem;
-        }
+        text-align: ${props => props.isPersian ? "right" : "left"};
+        word-spacing: 1px;
+        letter-spacing: -.5px;
+        word-break: break-all;
+        font-family: ${props => props.isPersian ? "Vazirmatn" : "Outfit"}, "Vazirmatn", sans-serif;
     }
 `;
 

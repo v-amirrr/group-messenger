7b93f261b07edec62afd042b4efcece1f5960204
Amir Valizadeh <79039612+v-amirrr@users.noreply.github.com>
2022-08-25 15:56:56 +0430

    feat: added the ability to delete any messages

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,12 +1,26 @@
-import React, { forwardRef } from 'react';
+import React, { forwardRef, useState } from 'react';
+
+import { db } from '../config/firebase';
+import { doc, deleteDoc } from "firebase/firestore";
+
+import { AiFillDelete } from 'react-icons/ai';
 
 import styled from 'styled-components';
+import { motion, AnimatePresence } from 'framer-motion';
+
+const menuVariants = {
+    hidden: { opacity: 0, scale: 0.6 },
+    visible: { opacity: 1, scale: 1, transition: { duration: 0.2, type: 'tween' } },
+    exit: { opacity: 0, scale: 0.6, transition: { duration: 0.2, type: 'tween' } }
+};
 
 const Message = forwardRef(( props, ref ) => {
 
     const localStorageUsername = JSON.parse(localStorage.getItem("username"));
 
     const { message, id, username } = props;
+
+    const [menuShow, setMenuShow] = useState(false);
 
     const isRTL = (text) => {           
         let ltrChars = 'A-Za-z\u00C0-\u00D6\u00D8-\u00F6\u00F8-\u02B8\u0300-\u0590\u0800-\u1FFF'+'\u2C00-\uFB1C\uFDFE-\uFE6F\uFEFD-\uFFFF',
@@ -15,11 +29,28 @@ const Message = forwardRef(( props, ref ) => {
         return rtlDirCheck.test(text);
     };
 
+    const deleteMessage = id => {
+        const docRef = doc(db, "messages", id);
+        deleteDoc(docRef);
+    };
+
     return (
         <>
-            <MessageBox ref={ref} key={id} isUser={username == localStorageUsername} isPersian={isRTL(message)}>
+            <MessageBox ref={ref} key={id} isUser={username == localStorageUsername} isPersian={isRTL(message)} onClick={() => setMenuShow(prevState => !prevState)}>
                 <p className='username' dir="auto">{username}</p>
                 <p className='message'>{message}</p>
+
+                <AnimatePresence>
+                    {
+                        menuShow
+                        &&
+                        <Menu isUser={username == localStorageUsername} key={id} initial='hidden' animate='visible' exit='exit' variants={menuVariants}>
+                            <div onClick={() => deleteMessage(id)}>
+                                <i><AiFillDelete /></i>
+                            </div>
+                        </Menu>
+                    }
+                </AnimatePresence>
             </MessageBox>
         </>
     );
@@ -57,6 +88,45 @@ const MessageBox = styled.div`
         letter-spacing: -.5px;
         word-break: break-all;
         font-family: ${props => props.isPersian ? "Vazirmatn" : "Outfit"}, "Vazirmatn", sans-serif;
+    }
+`;
+
+const Menu = styled(motion.div)`
+    position: absolute;
+    left: ${props => props.isUser && "-2.5rem"};
+    right: ${props => props.isUser || "-2.5rem"};
+    background-color: #ffffff22;
+    border-radius: 20px;
+    overflow: hidden;
+    z-index: 0;
+    user-select: none;
+
+    hr {
+        opacity: 0.2;
+    }
+    
+    div {
+        display: flex;
+        justify-content: flex-start;
+        align-items: center;
+        flex-direction: row;
+        width: 100%;
+        height: 50%;
+        cursor: pointer;
+        padding: .5rem;
+        transition: background .4s;
+
+        i {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-direction: row;
+            font-size: 1rem;
+        }
+
+        &:active {
+            background-color: #ffffff22;
+        }
     }
 `;
 

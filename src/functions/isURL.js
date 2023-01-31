0ff3a248c012f380c1b64f07bb5f13803a5b0026
Amir Valizadeh <79039612+v-amirrr@users.noreply.github.com>
2023-01-31 14:16:55 +0330

    fix: fixed the bug of isURL function
    Before the function detected '.' as a link, now it doesn't.

--- src/functions/isURL.js
+++ src/functions/isURL.js
@@ -1,11 +1,17 @@
 export const isURL = word => {
     let newWord = '';
+
+    if (/^\.*$/.test(word)) {
+        return { isLink: false, newWord: word };
+    }
+
     try {
         if (!/^https?:\/\//i.test(word)) {
             newWord = `http://${word}`;
         } else {
             newWord = word;
         }
+        
         const url = new URL(newWord);
         return url.hostname && url.hostname.includes('.') ? { isLink: true, newWord: newWord } : { isLink: false, newWord: word };
     } catch {
